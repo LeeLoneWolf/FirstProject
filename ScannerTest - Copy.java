5fad1084747bb6268d6e4d96dac6a00f6451085d
“[Bazith <[bazithali95@gmail.com]>
2017-12-19 09:41:03 +0530

    changes

---
+++ ScannerTest - Copy.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ScannerTest{
+ public static void main(String arg[])
+{
+	Scanner salary=new Scanner(System.in);
+	
+	
+
+	System.out.println("Enter salary");
+	float s=salary.nextFloat();
+
+        
+	float epf=(s/100*8);
+	float etf=(s/100*3);
+
+	
+
+	
+	System.out.println("Salary after ETF and EPF is " + (s-(epf+etf)));
+	System.out.println("EPF IS "+epf);
+	System.out.println("ETF IS "+etf);
+
+
+}
+}
